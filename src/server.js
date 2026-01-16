fed5cf0bfadcbea438e72653b8faaabfe76f714c
Asma-M06 <asmamadhvaswala52@gmail.com>
2026-01-16 12:47:39 +0530

    Added Username feature for users

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const redis = require('redis');
 const http = require('http');
-const {Server} = require('socket.io');
+const { Server } = require('socket.io');
 const { timeStamp } = require('console');
 require('dotenv').config();
 
@@ -12,7 +12,7 @@ const REDIS_URL = process.env.REDIS_URL || 'redis://localhost:6379';
 const publisher = redis.createClient({ url: REDIS_URL });
 const subscriber = publisher.duplicate();
 
-(async ()=>{
+(async () => {
     await publisher.connect();
     await subscriber.connect();
     console.log("Redis pub/sub connected");
@@ -20,26 +20,47 @@ const subscriber = publisher.duplicate();
 
 app.use(express.static('public'));
 
-io.on('connection',(socket)=>{
-    console.log('User connected : ',socket.id);
+io.on('connection', async (socket) => {
+    console.log('User connected : ', socket.id);
+    
+    socket.on('set_username', async (name) => {
+        socket.username = name;
+        console.log(`🏷️  User identified: ${name}`);
 
-    socket.on('send_message',async(data)=>{
-        const payload={
-            senderId : socket.id,
-            message : data.message,
-            timeStamp : new Date().toLocaleDateString()
+        try {
+            const history = await publisher.lRange('CHAT_HISTORY', 0, -1);
+            const parsedHistory = history.map(msg => JSON.parse(msg));
+            socket.emit('chat_history', parsedHistory);
+        } catch (err) {
+            console.error("History fetch error:", err);
+        }
+    });
+
+    socket.on('set_username',(name)=>{
+        socket.username = name;
+        console.log(`${socket.id} set name to ${name}`);
+    });
+
+    socket.on('send_message', async (data) => {
+        const payload = {
+            senderId: socket.username || 'GUEST',
+            message: data.message,
+            timeStamp: new Date().toLocaleDateString()
         };
 
-        await publisher.publish('CHAT_CLUSTER',JSON.stringify(payload));
+        await publisher.rPush('CHAT_HISTORY',JSON.stringify(payload));
+        await publisher.lTrim('CHAT_HISTORY', -50, -1);
+        await publisher.publish('CHAT_CLUSTER', JSON.stringify(payload));
     });
+    socket.on('disconnect', () => console.log('User disconnected'));
 });
 
 subscriber.subscribe('CHAT_CLUSTER', (message) => {
     const data = JSON.parse(message);
-    io.emit('receive_message',data);
+    io.emit('receive_message', data);
 });
 
 const PORT = process.env.PORT || 3001;
-server.listen(PORT,()=>{
+server.listen(PORT, () => {
     console.log(`Chat Server running on http://localhost:${PORT}`);
 });
